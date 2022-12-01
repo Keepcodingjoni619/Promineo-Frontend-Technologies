49056eafd7b370dd5a09f076523a3644fcc93af5
John Doe <johndoe@example.com>
2022-12-01 11:10:18 -0800

    made some changes

---
+++ Week-01-CLI_Source_Control_and_Variables/js/index.js
@@ -0,0 +1,4 @@
+let favoriteTeams = ["Los Angelos Lakers", "San Diego Chargers", "San Diego Padres"];
+console.log(favoriteTeams)
+favoriteTeams[0] = "San Diego Clippers";
+console.log(favoriteTeams)
