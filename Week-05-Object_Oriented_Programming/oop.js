04c69f84d7dc323cca47d8f5163a0ed789564e65
John Doe <johndoe@example.com>
2023-01-21 16:14:31 -0800

    created coding assignment folder

---
+++ Week-05-Object_Oriented_Programming/oop.js
@@ -0,0 +1,24 @@
+
+/*Factory Function */
+function createCircle(radius){
+    return {
+        radius,
+        draw: function(){
+            console.log('draw')
+        }
+    }
+};
+
+const circle = createCircle(1)
+console.log(circle)
+
+/* Constructor Function */ 
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw')
+    }
+};
+
+const another = new Circle(1)
+console.log(another)
